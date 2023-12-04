5796d083936da3c48e48dc95bd069b5cd103086f
Bikash Thapa <thapabikash48@gmail.com>
2023-12-04 10:48:12 +0000

    Final Submit

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,17 +1,44 @@
-// HomePage.js
+import React from 'react';
 import Product from '../components/product/Product';
 import productsData from '../products.json';
 
+function generateProducts(count) {
+  const products = [];
+  for (let i = 0; i < count; i++) {
+    const originalProduct = productsData.products[i % productsData.products.length];
+    const product = {
+      ...originalProduct,
+      id: i + 1,
+      thumbnail:originalProduct.thumbnail + "?s=" + i + 1, // Ensure unique IDs for generated products
+    };
+    products.push(product);
+  }
+  return products;
+}
+
 function HomePage() {
+  const generateProductsCount = 1000; // Adjust the count as needed
+  const generatedProducts = generateProducts(generateProductsCount);
+
+  const loadProductsStartTime = performance.now();
+
+  const productsDOM = generatedProducts.map(product => (
+    <div className="col-md-4 mb-4" key={product.id}>
+      <Product product={product} />
+    </div>
+  ));
+
+  const loadProductsEndTime = performance.now();
+
+
+  console.log('Generated products:', generatedProducts.length);
+
   return (
     <div className="container mt-5">
       <div className="row">
-        {productsData.products.map(product => (
-          <div className="col-md-4 mb-4" key={product.id}>
-            <Product product={product} />
-          </div>
-        ))}
+        {productsDOM}
       </div>
+      {console.log('Total DOM manipulation time for loading all products (in milliseconds):', loadProductsEndTime - loadProductsStartTime)}
     </div>
   );
 }
